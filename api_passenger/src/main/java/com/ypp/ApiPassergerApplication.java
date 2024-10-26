9d856c8614d0d1f0508a91d9d33ca8479c7ff597
73669 <736698755@qq.com>
2024-10-26 16:40:01 +0800

    骨架配置

---
+++ api_passenger/src/main/java/com/ypp/ApiPassergerApplication.java
@@ -0,0 +1,11 @@
+package com.ypp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//启动类
+@SpringBootApplication
+public class ApiPassergerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassergerApplication.class);
+    }
+}
