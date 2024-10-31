689642ffd98a2b28071ff481c5b8d4148e203646
73669 <736698755@qq.com>
2024-10-31 14:33:47 +0800

    添加验证码模块

--- api_passenger/src/main/java/com/ypp/ApiPassergerApplication.java
+++ api_passenger/src/main/java/com/ypp/ApiPassergerApplication.java
@@ -2,8 +2,11 @@ package com.ypp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 //启动类
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiPassergerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiPassergerApplication.class);
